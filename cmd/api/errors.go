5b1805e0ac1276d7bb2521b07968440a5669cd79
codytheroux96 <cody.theroux3@gmail.com>
2024-07-04 22:49:33 -0400

    made a generic logging helper that logs the method and url

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -4,3 +4,12 @@ import (
 	"fmt"
 	"net/http"
 )
+
+func (app *application) logError(r http.Request, err error) {
+	var (
+		method = r.Method
+		uri = r.URL.RequestURI()
+	)
+
+	app.logger.Error(err.Error(), "method", method, "uri", uri)
+}
