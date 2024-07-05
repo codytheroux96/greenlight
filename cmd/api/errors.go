bed8eaaaf7d4991119f374068789d4f74ae6dc6b
codytheroux96 <cody.theroux3@gmail.com>
2024-07-04 22:58:44 -0400

    wrote a logger helper that will send json formatted errors

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -5,11 +5,23 @@ import (
 	"net/http"
 )
 
-func (app *application) logError(r http.Request, err error) {
+func (app *application) logError(r *http.Request, err error) {
 	var (
 		method = r.Method
-		uri = r.URL.RequestURI()
+		uri    = r.URL.RequestURI()
 	)
 
 	app.logger.Error(err.Error(), "method", method, "uri", uri)
 }
+
+func (app *application) errorResponse(w http.ResponseWriter, r *http.Request, status int, message any) {
+	env := envelope{"error": message}
+
+	err := app.writeJSON(w, status, env, nil)
+	if err != nil {
+		app.logError(r, err)
+		w.WriteHeader(500)
+	}
+}
+
+
