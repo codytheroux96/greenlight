4f831b14fc72d61e69159960bee4c76f7f5bab5c
codytheroux96 <cody.theroux3@gmail.com>
2024-07-09 20:29:37 -0500

    fixed readJSON helper and added custom decoding

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -61,28 +61,36 @@ func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst any
 		switch {
 		case errors.As(err, &syntaxError):
 			return fmt.Errorf("body contains badly-formed JSON (at character %d)", syntaxError.Offset)
+
 		case errors.Is(err, io.ErrUnexpectedEOF):
-			return errors.New("body conatains badly-formed JSON")
+			return errors.New("body contains badly-formed JSON")
+
 		case errors.As(err, &unmarshalTypeError):
 			if unmarshalTypeError.Field != "" {
 				return fmt.Errorf("body contains incorrect JSON type for field %q", unmarshalTypeError.Field)
 			}
 			return fmt.Errorf("body contains incorrect JSON type (at character %d)", unmarshalTypeError.Offset)
+
 		case errors.Is(err, io.EOF):
 			return errors.New("body must not be empty")
+
 		case strings.HasPrefix(err.Error(), "json: unknown field "):
 			fieldName := strings.TrimPrefix(err.Error(), "json: unknown field ")
 			return fmt.Errorf("body contains unknown key %s", fieldName)
+
 		case errors.As(err, &maxBytesError):
 			return fmt.Errorf("body must not be larger than %d bytes", maxBytesError.Limit)
+
 		case errors.As(err, &invalidUnmarshalError):
 			panic(err)
+
 		default:
 			return err
 		}
 	}
+
 	err = dec.Decode(&struct{}{})
-	if err != nil {
+	if !errors.Is(err, io.EOF) {
 		return errors.New("body must only contain a single JSON value")
 	}
 
