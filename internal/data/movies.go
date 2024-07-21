ad2467ffc6f5c6c7a442d2b84e5789fc1b0a88ea
codytheroux96 <cody.theroux3@gmail.com>
2024-07-21 16:40:37 -0500

    creating a get method to get movies from the db

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -2,10 +2,11 @@ package data
 
 import (
 	"database/sql"
+	"errors"
 	"time"
 
-	"github.com/codytheroux96/greenlight/internal/validator"
 	"github.com/lib/pq"
+	"github.com/codytheroux96/greenlight/internal/validator"
 )
 
 type Movie struct {
@@ -51,7 +52,37 @@ func (m MovieModel) Insert(movie *Movie) error {
 }
 
 func (m MovieModel) Get(id int64) (*Movie, error) {
-	return nil, nil
+	if id < 1 {
+		return nil, ErrRecordNotFound
+	}
+
+	query := `
+	SELECT id, created_at, title, year, runtime, genres, version
+	FROM movies
+	WHERE id = $1`
+
+	var movie Movie
+
+	err := m.DB.QueryRow(query, id).Scan(
+        &movie.ID,
+        &movie.CreatedAt,
+        &movie.Title,
+        &movie.Year,
+        &movie.Runtime,
+        pq.Array(&movie.Genres),
+        &movie.Version,
+    )
+
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return nil, ErrRecordNotFound
+		default:
+			return nil, err
+		}
+	}
+
+	return &movie, nil
 }
 
 func (m MovieModel) Update(movie *Movie) error {
