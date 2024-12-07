c8a066126883dc510801386b6e2216fee60c6d5f
codytheroux96 <cody.theroux3@gmail.com>
2024-12-06 19:51:46 -0500

    fixing typos and incorrect struct tagging

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -15,10 +15,10 @@ type Filters struct {
 
 type Metadata struct {
 	CurrentPage  int `json:"current_page,omitempty"`
-	PageSize     int `json:"page_size,omitemmpty"`
-	FirstPage    int `json:"first_page,omitempty`
-	LastPage     int `json:"last_page,omitempty`
-	TotalRecords int `json:"total_records,omitempty`
+	PageSize     int `json:"page_size,omitempty"`
+	FirstPage    int `json:"first_page,omitempty"`
+	LastPage     int `json:"last_page,omitempty"`
+	TotalRecords int `json:"total_records,omitempty"`
 }
 
 func (f Filters) sortColumn() string {
